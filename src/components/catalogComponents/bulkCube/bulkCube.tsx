80b39fa6b5ab057bfa525240040b81b63ede93e5
framerage <officialigonin@mail.ru>
2023-04-12 13:54:27 +0400

    правка ссылки

--- src/components/catalogComponents/bulkCube/bulkCube.tsx
+++ src/components/catalogComponents/bulkCube/bulkCube.tsx
@@ -8,18 +8,21 @@ const BulkCube = () => {
     { id: 2, name: "bottom", color: "green" },
     { id: 3, name: "right", color: "orange" },
   ];
+  const wallStyle = (elem: number) => {
+    return {
+      transform: `rotateY(calc(90deg * ${figureWalls[elem].id} )) translateZ(150px)`,
+      backgroundColor: figureWalls[elem].color,
+    };
+  };
   return (
     <div className={classes.cubeContainer}>
       <div className={classes.cubeShadow}></div>
       <div className={classes.cube}>
-        {figureWalls.map((wall) => (
+        {figureWalls.map((wall, index) => (
           <span
             key={wall.name}
             className={classes.cubeWall}
-            style={{
-              ["--elem"]: `${wall.id}`,
-              backgroundColor: wall.color,
-            }}
+            style={wallStyle(index)}
           ></span>
         ))}
       </div>
